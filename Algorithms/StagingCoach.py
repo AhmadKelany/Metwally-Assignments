da2debf74d53165562c430ea07463f704bbdeed7
Ahmad Kelany <ahmad.kelany@yahoo.com>
2023-01-17 23:07:01 +0300

    progress

--- Algorithms/StagingCoach.py
+++ Algorithms/StagingCoach.py
@@ -14,17 +14,21 @@ data = [
 ]
 length = len(data)
 state = namedtuple('State' , ['From' , 'To' , 'Cost'])
-states = list(zip(labels, labels, labels))
+
+states = [state(l,l,0) for l in labels ]
 states[length-1] = state(From = 'J' ,To = 'J' ,Cost = 0)
 rowIndex = length - 2
 while(rowIndex >= 0):
-    states[rowIndex] = state(From=labels[rowIndex],To=labels[rowIndex+1],Cost=100000)
+    fromLabel = labels[rowIndex]
+    toLabel = ''
+    cost = 100000 
     for columnIndex in range(rowIndex+1,length):
         if data[rowIndex][columnIndex] == 0: continue
         newCost = data[rowIndex][columnIndex] + states[columnIndex].Cost
-        if newCost < states[rowIndex].Cost:
-            states[rowIndex].To = labels[columnIndex]
-            states[rowIndex].Cost = newCost 
+        if newCost < cost:
+            toLabel = labels[columnIndex]
+            cost = newCost 
+    
     rowIndex -=1
-
+    states[rowIndex] = state(fromLabel, toLabel , cost)
 print(states[0].Cost)
