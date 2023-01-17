746c530d190b70d5a0a25e6ae0a40398ef6aadb3
Ahmad Kelany <ahmad.kelany@yahoo.com>
2023-01-17 22:19:54 +0300

    progress

--- Algorithms/StagingCoach.py
+++ Algorithms/StagingCoach.py
@@ -1,3 +1,4 @@
+from collections import namedtuple
 labels = ['A' , 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']
 data = [
     [0,2,4,3,0,0,0,0,0,0],
@@ -11,3 +12,13 @@ data = [
     [0,0,0,0,0,0,0,0,0,4],
     [0,0,0,0,0,0,0,0,0,0],
 ]
+length = len(data)
+state = namedtuple('State' , ['From' , 'To' , 'Cost'])
+states = []
+states[length-1] = state(From = 'J' ,To = 'J' ,Cost = 0)
+rowIndex = length - 2
+while(rowIndex >= 0):
+    for columnIndex in range(rowIndex+1,length):
+        if data[rowIndex][columnIndex] == 0: continue
+        states[rowIndex] = state(From=labels[rowIndex],To=labels[columnIndex],Cost=100000)
+    rowIndex -=1
