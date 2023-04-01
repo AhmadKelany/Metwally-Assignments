1bd6579446ed2fec51aeb41c82976a2be4c15a28
Ahmad Kelany <ahmad.kelany@yahoo.com>
2023-04-01 06:09:19 +0300

    finished BFS

--- Algorithms/BreadthFirstSearch.py
+++ Algorithms/BreadthFirstSearch.py
@@ -11,6 +11,17 @@ graph = {
     'I' : ['G' , 'H']
 }
 q = queue.Queue()
-q.put(list(graph.keys())[0])
+first_v = list(graph.keys())[0]
+q.put(first_v)
 v_count = len(graph)
-visited = []
+visited = [first_v]
+current_v = first_v
+destinations = graph[first_v]
+while(not q.empty()):
+    current_v = q.get()
+    destinations = graph[current_v]
+    for i in range(len(destinations)):
+        if(not destinations[i] in visited):
+            q.put(destinations[i])
+            visited.append(destinations[i])
+            print(current_v , ' - ' , destinations[i])
